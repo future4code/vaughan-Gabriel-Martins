d19bc9ec7b432f7d2df9e4d5cbe8e6a28700e8ba
Gabriel Dias Lopes Martins <gdlmartins@Unknown.gateway>
2022-01-15 02:16:28 +0800

    first commit whatslab

---
+++ modulo2/projeto-whatslab/src/Components/MainContainer.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+
+const MainContainerBox = styled.div`
+   height:80vh; 
+   width: 40vh;
+   border: 1px solid black;
+   box-shadow: 0 0 50px rgba(42, 42, 42, 1);
+   margin: 5% auto;
+   position:relative;
+`
+
+const MainContainer = ({children, size}) => { 
+    
+    return ( 
+        <MainContainerBox> 
+         {children} 
+        </MainContainerBox>
+    )
+}
+
+
+export default MainContainer;
+
