03c8aac5c53410766f2d5640bf689bfe76fdaeac
Gabriel Dias Lopes Martins <gdlmartins@Gabriels-MBP.gateway>
2022-03-23 09:37:56 +0800

    node packega

---
+++ modulo3/usestate/insta-lab-hooks/build/precache-manifest.2521d585eaff619ae0150ff34c1535d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587f748196cca5f423a1f95cdec93029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e113aa322c870a433f61",
+    "url": "/static/js/2.ac3f5003.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ac3f5003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4958938b174f8bc3456c",
+    "url": "/static/js/main.f959ed47.chunk.js"
+  },
+  {
+    "revision": "fbdb0ec750bc986cd3d5",
+    "url": "/static/js/runtime-main.40261604.js"
+  },
+  {
+    "revision": "de299b47c15bad3a33ad36319a10bef3",
+    "url": "/static/media/comment_icon.de299b47.svg"
+  },
+  {
+    "revision": "042aa417f110588538aefef728ed90fa",
+    "url": "/static/media/favorite-white.042aa417.svg"
+  },
+  {
+    "revision": "4c67d8751e56b25704ac9b791835bef8",
+    "url": "/static/media/favorite.4c67d875.svg"
+  }
+]);
