eb4fd9ac573982959950d8e9c8149c43211d8ec2
Gabriel Dias Lopes Martins <gdlmartins@Gabriels-MBP.gateway>
2022-03-25 09:32:00 +0800

    1 done

--- modulo4/aprofundamento-typescript/Exercicio1/exercicio1.ts
+++ modulo4/aprofundamento-typescript/Exercicio1/exercicio1.ts
@@ -7,8 +7,8 @@ const minhaString: string = "10";
 // variável também aceite strings? Ou seja, como criamos no
 // typescript uma variável que aceite mais de um tipo de valor?
 
-const meuNumero : number | string  = "10"; 
-const meuNumero1 : number | string  = 10; 
+const meuNumero: number | string = "10";
+const meuNumero1: number | string = 10;
 
 
 // c) Agora crie um novo objeto. Este objeto é uma pessoa, e deve possuir três propriedades:
@@ -24,8 +24,59 @@ const meuNumero1 : number | string  = 10;
 
 //ie um **tipo** `Pessoa` para garantir que todos os objetos tenham os mesmos campos.
 
-// type pessoa : { nome: string , 
-//     idade: number, 
-//     corFavorita : string
+type pessoa = {
+    nome: string,
+    idade: number,
+    corFavorita: string
+}
 
-// } 
+const Gabriel : pessoa = { 
+    nome : "Gabriel", 
+    idade: 19, 
+    corFavorita :"preta",
+}
+
+const Jieun : pessoa ={
+    nome: "Jieun",
+    idade: 35,
+    corFavorita: "Vermelho",
+}
+
+const Velho :pessoa ={
+    nome: "Andre",
+    idade: 40,
+    corFavorita: "Cinza"
+}
+
+// d) Modifique a propriedade corFavorita para que apenas seja possível 
+// escolher entre as cores do arco-íris. Utilize um enum para isso.
+
+enum corFavorita  {  vermelha = "vemelha" , 
+    laranja ="Laranja", 
+    amarela = " Amarela", 
+    verde = "Verde",
+     azul = "Azul", 
+     azulEscuro = "azulEscuro",
+    violeta = "violeta"
+     
+}
+
+type pessoaD = {
+    nome: string,
+    idade: number,
+    corFavorita: corFavorita
+}
+
+const JoaoD : pessoaD =  {
+    nome: "Joao",
+    idade: 0,
+    corFavorita: corFavorita.azul
+}
+const Joao : pessoa =  {
+    nome: "Joao",
+    idade: 0,
+    corFavorita: "azul"
+}
+
+console.log("Joao",Joao )
+console.log("JoaoD", JoaoD)
