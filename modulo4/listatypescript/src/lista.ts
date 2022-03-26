930c225b3157007316f4a867ac36df8ad8492763
Gabriel Dias Lopes Martins <gdlmartins@Unknown.gateway>
2022-03-26 18:54:02 +0800

    6

--- modulo4/listatypescript/src/lista.ts
+++ modulo4/listatypescript/src/lista.ts
@@ -186,3 +186,44 @@ if(process.argv[2]==="5"){
  console.table(exercicio5(arrayEx5))
  
 }
+
+
+// - Exercício 6
+    
+//     Agora, pediram para você ajudar a fazer uma funcionalidade de 
+// 	um banco digital. Antes de explicar a sua tarefa, você precisa 
+// 	entender como eles guardam as contas dos clientes. 
+// 	Basicamente, eles salvam o nome do clientes, o saldo total e 
+// 	uma lista contendo todas os débitos realizados pelo cliente. 
+// Pensando em aumentar seu lucros, o banco quer identificar possíveis 
+// clientes precisando de empréstimos. Dessa forma, a sua tarefa é criar 
+// uma função que receba este array como parâmetro, atualize o saldo total 
+// descontando todos os débitos e retorne apenas os clientes com saldo negativo.
+	
+// 	Exemplo:
+
+const entradaEx6 = [
+	{ cliente: "João", saldoTotal: 1000, debitos: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, debitos: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, debitos: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, debitos: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, debitos: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, debitos: [] }
+]
+type  input  = 	{ cliente: string , saldoTotal: number, debitos: number [] }
+
+const exercicio6 = (entrada : input[] ): string[] => {  
+    return entrada
+	
+	.filter((usuario )=> {  
+		if (usuario.saldoTotal -  usuario.debitos.reduce((sum, cur)=> sum+cur , 0) >= 0 ) { 
+			return false 
+		}else return true 	
+	}) .map((usuario)=>usuario.cliente )
+
+
+}
+
+if(process.argv[2]==="6"){ 
+	console.table(exercicio6(entradaEx6))
+   }
