f7e99f79805ba307f74d06f94d1a483f32fa4288
Gabriel Dias Lopes Martins <gdlmartins@Gabriels-MBP.gateway>
2022-03-31 17:51:19 +0800

    todos os exercicios

--- modulo4/fluxo-de-dados-backend/src/exercicios.ts
+++ modulo4/fluxo-de-dados-backend/src/exercicios.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import cors from "cors"; 
 
 //Exercício 2   
 // Crie um arquivo chamado **`data.ts`** que exporta um array de produtos. Cada produto será representado por um objeto com propriedades: id (`string`), name (`string`) e price (`number`). 
 // Popule manualmente o array com pelo menos 3 produtos.
-import {data} from "./data"
+import { data } from "./data"
 import { exec } from "child_process";
+import { error } from "console";
 
 
 const app = express();
@@ -20,7 +20,7 @@ const gate = 3003;
 // de teste com método `**GET`** no path **`“/test”`** que retorna uma mensagem genérica 
 // avisando que a API está funcional.
 
-app.get(("/test"), (req , res )=>{
+app.get(("/test"), (req, res) => {
     res.send(`A api ta funcionando`)
 })
 
@@ -31,14 +31,14 @@ app.get(("/test"), (req , res )=>{
 // })
 
 // - Exercício 3
-    
+
 // Desenvolva um endpoint que **cria um novo produto** e 
 // retorna a lista de produtos atualizada.
 // A id do produto deve ser gerada automaticamente pela API.
 
 
-app.post(("/products"), (req,res)=> {
-    const arrayData = [...data,{id:Date.now(), ...req.body}]
+app.post(("/products"), (req, res) => {
+    const arrayData = [...data, { id: Date.now(), ...req.body }]
     res.status(201).send(arrayData)
 })
 
@@ -47,7 +47,7 @@ app.post(("/products"), (req,res)=> {
 
 //     Crie um endpoint que **retorna todos os produtos**.
 
-app.get(("/products"), (req , res )=>{
+app.get(("/products"), (req, res) => {
     res.status(200).send(data)
 
 })
@@ -58,50 +58,186 @@ app.get(("/products"), (req , res )=>{
 // Crie um endpoint que **edita** **o preço de** 
 // **um determinado produto** e retorna a lista de produtos atualizada.
 
-app.put(("/products"),(req,res)=> { 
-    
-  const dataPriceChanges = data.map(item => {
-       if(Number(item.id) === req.body.id){ 
-           return {...item, price: req.body.price }
-       }else return item 
-  
-  })
-    
- res.send(dataPriceChanges)
+app.put(("/products"), (req, res) => {
+
+    const dataPriceChanges = data.map(item => {
+        if (Number(item.id) === req.body.id) {
+            return { ...item, price: req.body.price }
+        } else return item
+
+    })
+
+    res.send(dataPriceChanges)
 })
 
-app.delete(("/products"),(req,res) => {
-    const datafiltered = data.filter(item =>  !(Number(item.id )=== req.body.id ))
-   
+
+// Exercicos 6
+app.delete(("/products"), (req, res) => {
+    const datafiltered = data.filter(item => !(Number(item.id) === req.body.id))
+
     res.status(201).send(datafiltered)
 })
 
 
 
 // - Exercício 7
-    
+
 //     Refatore o endpoint do exercício 3 (criar produto) para que sejam 
 // implementados fluxos de validação dos dados recebidos (`name` e `price`)
 
-app.post(("/produtos"), (req,res)=> {
-//  erro caso um ou nenhum deles forem recebidos
-    
-    if(req.body.price || req.body.name){ 
-        console.log("recebi os dois")
+app.post(("/produtos"), (req, res) => {
+    try {
+        //  erro caso um ou nenhum deles forem recebidos
+        if (req.body.price === '' || req.body.name === '') {
+            console.log(req.body.price, req.body.name)
+            throw Error("Não recebi preço ou/e nome do produto")
+        }
+        //   erro caso `name` seja diferente de `string`
+        if (typeof req.body.name !== "string") {
+            throw Error("Não esta é um formato valido 'não é string' ")
+        }
+        //     - erro caso `price` seja diferente de `number`
+        if (typeof req.body.price !== "number") {
+            throw Error("Não esta é um formato valido 'não é um numero' ")
+        }
+        //     - erro caso `price` seja igual ou menor que `0`
+        if (req.body.price <= 0) {
+            throw Error("Preço invalido")
+        }
     }
-     
-    const arrayData = [...data,{id:Date.now(), ...req.body}]
+
+    //     - erro caso algo inesperado aconteça  
+    // O catch vai pegar outros erros. 
+    catch (error: any) {
+        switch (error.message) {
+            case "Não esta é um formato valido 'não é string' ":
+            case "Não esta é um formato valido 'não é um numero' ":
+            case "Não recebi preço ou/e nome do produto":
+            case "Preço invalido":
+                res.status(422).send(error.message)
+                break;
+            default:
+                res.status(500).send(error.message)
+
+        }
+
+    }
+
+
+    const arrayData = [...data, { id: Date.now(), ...req.body }]
     res.status(201).send(arrayData)
 })
-//     - erro caso `name` seja diferente de `string`
-//     - erro caso `price` seja diferente de `number`
-//     - erro caso `price` seja igual ou menor que `0`
-//     - erro caso algo inesperado aconteça
+
+// - Exercício 8
+
+// Refatore o endpoint do exercício 5 (editar produto) 
+// para que sejam implementados fluxos de validação dos dados recebidos (`price`)
+
+// { para nao ter conflito com a questao 5 mudei o url para produtos }
+
+app.put(("/produtos"), (req, res) => {
+    // - erro caso algo inesperado aconteça
+
+    try {
+        // - erro caso `price` não seja recebido
+        if (!req.body.price) {
+            throw Error("Preço inexistente")
+        }
+        // - erro caso `price` seja diferente de `number`
+        if (typeof req.body.price !== "number") {
+            throw Error("Preço não é um numero")
+        }
+        // - erro caso `price` seja igual ou menor que `0`
+        if (req.body.price <= 0) {
+            throw Error("Preço menor ou igual a 0. ")
+        }
+        // - erro caso o produto a ser editado não seja encontrado       
+        const id = ((req.body.id).toString())
+
+        if (data.filter(item => item.id === id).length === 0) {
+            throw Error("Produto não encontrado")
+        }
+
+        const dataPriceChanges = data.map(item => {
+            if (Number(item.id) === req.body.id) {
+                return { ...item, price: req.body.price }
+            } else return item
+        })
+
+
+
+        res.send(dataPriceChanges)
+    }
+    catch (error: any) {
+        switch (error.message) {
+            case "Preço inexistente":
+                res.status(409).send(error.message)
+                break;
+            case "Preço não é um numero":
+                res.status(400).send(error.message)
+                break;
+            case "Produto não encontrado":
+                res.status(400).send(error.message)
+                break;
+            case "Preço menor ou igual a 0. ":
+                res.status(42).send(error.message)
+                break;
+            default:
+                res.send(error.message)
+
+        }
+
+
+    }
+
+
+})
 
 
 
 
+// app.delete(("/products"), (req, res) => {
+//     const datafiltered = data.filter(item => !(Number(item.id) === req.body.id))
 
-const server = app.listen(gate, ()=> { 
-   console.log( "The server is running at:", gate)
+//     res.status(201).send(datafiltered)
+// })
+
+
+// - Exercício 9
+    
+// Refatore o endpoint do exercício 6 (deletar produto) para que sejam implementados 
+// fluxos de validação dos dados recebidos
+    
+// - erro caso o produto escolhido não seja encontrado
+//  - erro caso algo inesperado aconteça
+
+
+
+
+app.delete(("/produtos"), (req, res) => {
+    
+    const datafiltered = data.filter(item => (Number(item.id) === req.body.id)) 
+    try{  
+    if(datafiltered.length <=0 ){ 
+        throw Error("Produto não encontrado")
+    }
+    res.status(201).send(datafiltered)
+    
+} catch(error: any){
+    switch(error.message){
+        case "Produto não encontrado" : 
+           res.status(400).send(error.message)
+           break;
+        default: 
+           res.status(500).send(error.messge)
+         
+        }
+
+     }
+
+})
+
+
+const server = app.listen(gate, () => {
+    console.log("The server is running at:", gate)
 })
