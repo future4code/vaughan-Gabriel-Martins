1ec840174de175b54adad9aee6716320375aebd4
Gabriel Dias Lopes Martins <gdlmartins@Gabriels-MBP.gateway>
2022-04-04 17:41:21 +0800

    desafios ate o 3

--- modulo4/labebank/src/index.ts
+++ modulo4/labebank/src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { isNotEmittedStatement, visitFunctionBody } from "typescript";
+import { NOMEM } from "dns";
 
 console.log("teste")
 
@@ -169,23 +171,40 @@ app.get("/users" , (req:Request , res: Response)=> {
 app.put("/users",  ( req:Request , res:Response)=> { 
     // Entrar com ano mes e dia  aaaa-mm-dd  
     // 18 anos em ms   567993600000 pelo calculo abaixo  18years  coverted in  ms => 568036800000
-
+    
+    
+  //*** Desafios 1 Adicione mais uma validação na função do item 1 (Criar conta): 
+    // verifiquem se o CPF passado já não está atrelado a alguma conta existente.
      
     try{
-        const dob = Date.parse(req.body.dob)
+       //cpf  
+       let  {cpf , dob }  = req.body 
+       if(usuarios.filter(item => item.cpf === cpf).length > 0){ 
+        throw new Error("cpf ja cadastrado")
+       }
+
+      // idade 
+        dob = Date.parse(dob)
         const today = (Date.parse((new Date(Date.now())).toDateString()))
         // const isOver18 = (today - dob) >=  567993600000
         const isOver18 = (today - dob) >=  568036800000
         if(isOver18){ 
             usuarios.push(req.body)
-          return res.status(200).send( usuarios )
+          // return res.status(200).send(req.body )
+          return res.status(200).send(usuarios )
         }
          throw new Error("Não tem a idade minima para abrir conta")
+       // idade end 
+
+
     }   
     catch(error:any) { 
       switch(error.message) { 
         case "Não tem a idade minima para abrir conta":
              res.status(400).send(error.message)
+             break;
+        case "cpf ja cadastrado":
+             res.status(403).send(error.message)
              break;
         default: 
               res.status(500).send(error.message);
@@ -195,6 +214,104 @@ app.put("/users",  ( req:Request , res:Response)=> {
            
     
 } )  
+
+// **** DESAFIOS o 1 esta acima 
+
+// Crie um endpoint get que receba um CPF como parâmetro 
+// e retorne o saldo da conta do usuário dono daquele CPF. 
+// A informação deve ser igual ao que estiver salvo no sistema. 
+// Se for diferente, exiba uma mensagem de erro.
+
+
+let desafiosArray = [
+  {
+    nome: 'Joe Doe usuario',
+    cpf: 22222222222,
+    dob:"2000-01-01",
+    saldo: 10,
+    extrato: [
+      {
+        valor: 100,
+        data: "2022-01-01",
+        descricao: 'Deposito inicial'
+      }
+    ],
+    transacoes: [
+      {
+        valor: 100,
+        descricao: 'escola',
+        datapagamento: "2022-04-10"
+      }
+    ]
+  },
+  {
+    nome: 'Seila',
+    cpf: 11111111111,
+    dob:"2000-01-02",
+    saldo: 20,
+    extrato: [
+      {
+        valor: 100,
+        data: "2022-01-01",
+        descricao: 'Deposito inicial'
+      }
+    ],
+    transacoes: [
+      {
+        valor: 100,
+        descricao: 'escola',
+        datapagamento: "2022-04-10"
+      }
+    ]
+  }
+]
+
+
+
+app.get("/users/:cpf" , (req:Request, res:Response)=> { 
+    const saldo  = desafiosArray
+    .filter(item => item.cpf === Number(req.params.cpf))
+    .map(item =>{ return {saldo:  item.saldo}})
+
+    res.status(200).send(saldo)
+})
+  
+// 3) Crie um endpoint put que receba um nome, um CPF e um valor. 
+// Ele deve adicionar o valor ao saldo do usuário. O nome e o CPF 
+// devem ser iguais ao que estiver salvo no sistema Se algum dos dados 
+// for diferente, exiba uma mensagem de erro.
+
+
+app.put("/user/saldo", ( req: Request , res: Response)=>{
+
+  try{
+    const {nome, cpf, saldo} = req.body;
+    console.log( nome,cpf,saldo)
+
+   const  isNomeCpfRight = desafiosArray.filter(item => (item.nome === nome && item.cpf === cpf))
+    if(isNomeCpfRight.length > 0){
+     desafiosArray =  desafiosArray.map( item => { 
+    //  const NewdesafiosArray =  desafiosArray.map( item => { 
+       if(item.cpf === cpf) { 
+         return { ...item , "saldo" : item.saldo + saldo}
+       }else return item  })
+      res.status(201).send(desafiosArray)
+    } else throw new Error("Esta conta  não existe")
+  
+  }
+  catch(e:any){
+    switch(e.message){ 
+      case "Esta conta  não existe": 
+           res.status(422).send(e.message)
+      default: 
+           res.status(500).send(e.message)
+           
+    }
+
+   }
+   
+
+})
 
 
 
