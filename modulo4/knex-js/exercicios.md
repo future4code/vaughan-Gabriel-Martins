bb10c1b258c97babfac732578d1f15b9c530ab7c
Gabriel Dias Lopes Martins <gdlmartins@Gabriels-MBP.gateway>
2022-04-07 16:28:19 +0800

    3

--- modulo4/knex-js/exercicios.md
+++ modulo4/knex-js/exercicios.md
@@ -92,3 +92,144 @@ c) *Faça uma função que receba um `gender` retorne a quantidade de itens na t
 
 
 ```
+
+## Exercicio2 
+
+```
+a) Uma função que receba um salário e um id e realiza a atualização do salário do ator em questão
+````
+```
+            const salario = async(salary:number, id: string): Promise<void> => {
+                await connection("Actor")
+                .update({ salary})
+                .where("id", id );
+
+            }
+
+            app.put("/users/:id" , (req: Request , res: Response) => { 
+                const id: string   = req.params.id;
+                const salary:number = req.body.salary;
+                salario(salary, id )
+                console.log( salary , id)
+                res.send()
+            } )
+
+
+```
+
+```
+b) Uma função que receba um id e delete um ator da tabela
+```
+
+ const deleteAtor = async(id:string): Promise<void> =>{ 
+     await connection("Actor")
+     .delete()
+     .where("id", id);
+ }
+
+app.delete("/users" , (req:Request , res: Response)=> {
+     const id = req.body.id;
+     console.log(req.body.id)
+     deleteAtor(id)
+     res.status(200).send(`Deletado`)
+
+})
+
+
+
+```
+c) *Uma função que receba um `gender` e devolva a média dos salários de atrizes ou atores desse `gender`*
+```
+
+```
+
+    const salarioMedioPorSexo = async (gender: string): Promise<any> => {
+        const result = await connection("Actor")
+          .avg("salary as average")
+          .where( "gender", gender);
+      
+        return result[0].average;
+      };
+
+
+<!-- app.get("/user/:gender" , (req:Request , res:Response) => {
+    const gender = req.body.gender;
+    console.log(gender);
+    const salarioMedio =  salarioMedioPorSexo(gender)
+    console.log(salarioMedio)
+    res.send(salarioMedio)
+
+}) -->
+    const salarioMedioPorSexo = async (gender: string): Promise<any> => {
+        const result = await connection("Actor")
+          .avg("salary")
+          .where( "gender", gender);
+      
+        return result[0];
+      };
+
+
+```
+
+## Exercicio 3 
+
+```
+const pegaAtor =async (id:string): Promise<any>=> { 
+  const result =  await connection("Actor")
+    .select()
+    .where("id", id)
+   return result[0]
+};
+
+
+
+app.get("/actor/:id", async(req:Request, res: Response)=> { 
+    try { 
+        const pegarAtorGet = await pegaAtor(req.params.id )
+        res.status(200).send(pegarAtorGet)
+        console.log("teste")
+
+    }
+    catch(e:any){
+        res.status(400).send({message: e.message })
+
+    }
+});
+
+
+```
+b) Crie um endpoint agora com as seguintes especificações:
+
+- Deve ser um GET (`/actor`)
+- Receber o gênero como um *query param* (`/actor?gender=`)
+- Devolver a quantidade de atores/atrizes desse gênero
+```
+
+ const numeroSexo = async (gender:string): Promise<any> => { 
+     const result = await connection("Actor")
+     .count("gender as quantidade" )
+     .where("gender" , gender)
+     return result[0]
+ }
+
+
+app.get("/actor" ,async (req:Request , res : Response)=> { 
+   try{ 
+    const gender = req.query.gender as string 
+    const numeroPorSexoGet  = await numeroSexo(gender)
+    res.status(201).send(numeroPorSexoGet)
+
+   }
+  catch(e:any ){ 
+      res.status(400).send({message: e.message })
+
+  }
+
+})
+
+
+```
+
+
+
+
--- modulo4/knex-js/src/index.ts
+++ modulo4/knex-js/src/index.ts
@@ -3,6 +3,7 @@ import knex from 'knex';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import {AddressInfo} from "net";
+import { send } from 'process';
 
 
 dotenv.config(); 
@@ -66,6 +67,136 @@ export const connection = knex({
 //     }
 //   })
 
+// const createActor = async ( 
+//     id: string, 
+//     name: string, 
+//     salary: number, 
+//     gender: string, 
+//     dateOfBirth: Date
+// ): Promise<void> => { 
+// await connection
+//     .insert({
+//     id:id,
+//     name: name, 
+//     salary: salary,
+//     brith_date:dateOfBirth, 
+//     gender:gender,
+//     })
+//     .into("Actor");
+// };
+
+// const salario = async(salary:number, id: string): Promise<void> => {
+//     await connection("Actor")
+//     .update({ salary})
+//     .where("id", id );
+
+// }
+
+//  app.put("/users/:id" , (req: Request , res: Response) => { 
+//      const id: string   = req.params.id;
+//      const salary:number = req.body.salary;
+//      salario(salary, id )
+//      console.log( salary , id)
+//      res.send()
+//  } )
+
+
+
+//  const deleteAtor = async(id:string): Promise<void> =>{ 
+//      await connection("Actor")
+//      .delete()
+//      .where("id", id);
+//  }
+
+// app.delete("/users" , (req:Request , res: Response)=> {
+//      const id = req.body.id;
+//      console.log(req.body.id)
+//      deleteAtor(id)
+//      res.status(200).send(`Deletado`)
+
+// })
+
+//     const salarioMedioPorSexo = async (gender: string): Promise<any> => {
+//         const result = await connection("Actor")
+//           .avg("salary") 
+//           .where( "gender", gender);
+//         return result[0];
+//       };
+
+//       salarioMedioPorSexo("female")
+// 	.then(result => {
+// 		console.log(result)
+// 	})
+// 	.catch(err => {
+// 		console.log(err)
+// 	});
+
+
+
+// app.get("/user" , (req:Request , res:Response) => {
+//     const gender = req.body.gender;
+//     console.log(gender);
+
+
+//     salarioMedioPorSexo(gender)
+//     .then( result =>  
+        
+//         res.send(result)
+//     ).catch(err => {
+// 		console.log(err)
+// 	});
+
+
+// })
+
+
+
+const pegaAtor =async (id:string): Promise<any>=> { 
+  const result =  await connection("Actor")
+    .select()
+    .where("id", id)
+   return result[0]
+};
+
+
+
+app.get("/actor/:id", async(req:Request, res: Response)=> { 
+    try { 
+        const pegarAtorGet = await pegaAtor(req.params.id )
+        res.status(200).send(pegarAtorGet)
+        console.log("teste")
+
+    }
+    catch(e:any){
+        res.status(400).send({message: e.message })
+
+    }
+});
+
+
+ const numeroSexo = async (gender:string): Promise<any> => { 
+     const result = await connection("Actor")
+     .count("gender as quantidade" )
+     .where("gender" , gender)
+     return result[0]
+ }
+
+
+app.get("/actor" ,async (req:Request , res : Response)=> { 
+   try{ 
+    const gender = req.query.gender as string 
+    const numeroPorSexoGet  = await numeroSexo(gender)
+    res.status(201).send(numeroPorSexoGet)
+
+   }
+  catch(e:any ){ 
+      res.status(400).send({message: e.message })
+
+  }
+
+
+
+})
 
 
 const server = app.listen(process.env.PORT || 3003, ()=>{
@@ -75,3 +206,7 @@ const server = app.listen(process.env.PORT || 3003, ()=>{
      console.log(`Server has failed`)
  }
 });
+
+
+
+
