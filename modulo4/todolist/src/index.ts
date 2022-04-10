2fdc7c50fd2b86874d66d2a654beca251be80f5f
Gabriel Dias Lopes Martins <gdlmartins@Gabriels-MBP.gateway>
2022-04-10 15:36:29 +0800

    ate  o desafio  7

--- modulo4/todolist/src/index.ts
+++ modulo4/todolist/src/index.ts
@@ -3,7 +3,7 @@ import express, { Response, Request, response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import knex from "knex";
-import { getImpliedNodeFormatForFile } from "typescript";
+
 
 
 dotenv.config();
@@ -26,38 +26,32 @@ const connection = knex({
 });
 
 
-// const getActorById = async (name: string) =>{ 
-//     const result = await connection.raw(`
-//     SELECT * FROM Actor WHERE name = "${name}"
-// `)
-// return result [0][0]
-// }
 
-
-// getActorById("Gabriel Martins")
-// .then(result => {
-//     console.log(result)
-// })
-// .catch(err => {
-//     console.log(err)
-// });
-
-
-
-//1  CREATTING AN USER 
+//1  CREATING AN USER 
 type body1 = { name: string, nickname: string, email: string }
 
-app.post("/user", (req: Request, res: Response) => {
+const creatingUser = async (id: string, name: string, nickname: string, email: string): Promise<any> => {
+    const result = await connection("TodoListUser")
+        .insert({ id, name, nickname, email })
+
+    return result;
+
+}
+
+app.post("/user", async (req: Request, res: Response) => {
     try {
         const { name, nickname, email }: body1 = req.body;
         if (!name || !nickname || !email) {
             throw new Error("Um ou mais entradas está vazia")
         }
-        if (typeof name === "string" || typeof email === "string" || typeof nickname === "string") {
+        if (typeof name !== "string" || typeof email !== "string" || typeof nickname !== "string") {
             throw new Error("Um ou mais entradas não são validas")
         }
         const id: string = (Date.now().toString())
-        res.status(200).send({ "id": id, name, email, nickname })
+        const resultCreating = await creatingUser(id, name, email, nickname)
+        const resultUserId = await getUserById(id);
+        // res.status(200).send({ "id": id, name, email, nickname })
+        res.status(200).send(resultUserId)
     }
     catch (e: any) {
         switch (e.message) {
@@ -83,26 +77,26 @@ const getUserById = async (id: string): Promise<any> => {
     return result;
 }
 
-app.get("/user/:id", async (req: Request, res: Response) => {
-    try {
-        const id = req.params.id;
-        const result = await getUserById(id)
-        console.log("result", result)
-        if (result.length > 0) {
-            res.status(200).send(result);
-        } else throw new Error("Este usuario não existe!")
-    }
-    catch (e: any) {
-        switch (e.message) {
-            case "Este usuario não existe!":
-                res.status(400).send(e.message)
-                break;
-            default:
-                res.status(500).send(e.message);
-        }
-    }
-}
-)
+// app.get("/user/:id", async (req: Request, res: Response) => {
+//     try {
+//         const id = req.params.id;
+//         const result = await getUserById(id)
+//         console.log("result", result)
+//         if (result.length > 0) {
+//             res.status(200).send(result);
+//         } else throw new Error("Este usuario não existe!")
+//     }
+//     catch (e: any) {
+//         switch (e.message) {
+//             case "Este usuario não existe!":
+//                 res.status(400).send(e.message)
+//                 break;
+//             default:
+//                 res.status(500).send(e.message);
+//         }
+//     }
+// }
+// )
 
 
 
@@ -121,63 +115,149 @@ type body2 = { name: string, nickname: string }
 
 app.put("/user/edit/:id", async (req: Request, res: Response) => {
     try {
-        const id = req.params.id;
         const { name, nickname }: body2 = req.body;
-        // const nickname = "SuperAstro"
-        const result = await changeUserNameNickName(id, name, nickname)
-        if (result === 0) {
-            throw new Error("It didn't update it")
-        } else res.status(201).send("Changed")
+        const id = req.params.id;
+        if (name && nickname) {
+            const result = await changeUserNameNickName(id, name, nickname)
+            if (result === 0) {
+                throw new Error("It didn't update it, id not found!")
+            } else res.status(201).send("Changed")
+        } else throw new Error("Uma das entradas não é valida!")
+
     } catch (e: any) {
-        switch(e.message){ 
-            case "It didn't update it":
-                res.status(201).send(e.message);
+        switch (e.message) {
+            case "It didn't update it, id not found!":
+                res.status(400).send(e.message);
                 break;
-            default: 
+            case "It didn't update it":
+                res.status(422).send(e.message);
+                break;
+            default:
                 res.status(500).send(e.message)
-                
         }
-
-
     }
 }
 )
 
 
+//4  Creating task 
+type task = { title: string, description: string, limitDate: string, creatorUserId: string }
 
+// Funcao de criação de task no Banco de dados. 
+const creatingTask = async (id: string, title: string, description: string, limitDate: string, creatorUserId: string)
+    : Promise<any> => {
+    const result =
+        await connection("TodoListTask")
+            .insert({
+                id: id,
+                "Title": title,
+                "Description": description,
+                "Limit_date": limitDate,
+                "Creator_user_id": creatorUserId
+            })
+    return result;
+}
 
+app.post("/task", async (req: Request, res: Response) => {
 
-// Creating task 
-type task = { title: String, description: String, limitDate: String, creatorUserId: String }
+    try {
+        const { title, description, limitDate, creatorUserId }: task = req.body;
+        if (title && description && limitDate && creatorUserId) {
 
-app.post("/task", (req: Request, res: Response) => {
-    const { title, description, limitDate, creatorUserId }: task = req.body
-    res.status(201).send({ title, description, limitDate, creatorUserId })
+            //Convertendo a data para o banco de dados.
+            const limiteDateFormated = limitDate.split("/").reverse().join("-");
+
+            // Criando um id unica a partir da data atual e transformando em string por causa do "type" do id. 
+            const id: string = Date.now().toString();
+
+            // Criado a task no banco de dados usando a funcao creatingTask  
+            const result1 = await creatingTask(id, title, description, limiteDateFormated, creatorUserId);
+
+            // Chamei a funcao d getting task by id para mandar a nova task de volta na API.
+            // Sei que nao foi pedido mas so mandando para comecar a ter uma maior overview. 
+            const result2 = await getTaskbyId(id)
+
+            res.status(201).send(result2[0])
+        } else throw new Error("Uma ou mais entradas não são validas");
+
+    }
+    catch (e: any) {
+        switch (e.message) {
+            case "Uma ou mais entradas não são validas":
+                res.status(422).send(e.message);
+                break;
+            default:
+                res.status(500).send(e.message);
+        }
+
+    }
 })
 
+// 5 Getting task by Id 
+const getTaskbyId = async (id: string): Promise<any> => {
+    const result = await connection
+        .select("*")
+        .from("TodoListTask")
+        .where({ id: id })
+    return result;
+}
+
+app.get("/task/:id", async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const result = await getTaskbyId(id);
+        console.log("result5", result)
+        if (id && result.length > 0) {
+            res.status(200).send(result[0]);
+        } else throw new Error("Esta id não foi encontada!")
+    }
+    catch (e: any) {
+        switch (e.message) {
+            case "Esta id não foi encontada!":
+                res.status(400).send(e.message)
+                break;
+            default:
+                res.status(500).send(e.message)
+        }
+    }
+})
+
+
+//  *** Desafios 
+
+/// Exercicio 6   lembre de comentar o exercicio 2 pois o url eh users/:id  
+// este app.get teria que vir antes dele mas coloquei aqui para seguir a ordem 
+// cronologica da lista. 
+
+// Pegandando todos os usuarios 
+const getAllUser = async (): Promise<any> => {
+    const result = await connection("TodoListUserEmptyForTest")
+        .select("id", "nickname")
+    return result;
+}
+
+//
+
+app.get("/user/all", async (req: Request, res: Response) => {
+    const result = await getAllUser();
+    console.log(result);
+    if (result.length > 0) {
+        res.send({ users: result })
+        // Pelo que eu entendi quer um array  []   e se fosse so {users : []} 
+        // eu nao colocaria o if.   assim fica so o "[]" 
+    } else res.send([]);
+})
+
+
+
+
+
+
+
+
+
+// Server 
 const server = app.listen(3003, () => {
     console.log("****server is running *****")
     console.log("****** to do list ******")
-})
-
-// Getting task by Id 
-
-console.log("teste2")
-
-const getuserbyid = async (id: string): Promise<any> => {
-    const result = await connection
-        .select("*")
-        .from("TodoListUser")
-        .where({ id: id })
-
-    return result;
-}
-const ver = async () => {
-    const result = await getuserbyid("001")
-    console.log(result)
-}
-
-app.get("/task/:id", (req: Request, res: Response) => {
-    const id = req.params.id;
-    res.send(id);
 })
