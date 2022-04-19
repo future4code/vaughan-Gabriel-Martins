4d940ef10f7f7f39d357f91f0bbdbed16765e12d
Gabriel Dias Lopes Martins <gdlmartins@Gabriels-MBP.gateway>
2022-04-19 11:16:36 +0800

    pronto

--- modulo4/classes-encapsulamento/src/endpoints/RespostasConceituais.md
+++ modulo4/classes-encapsulamento/src/endpoints/RespostasConceituais.md
@@ -7,11 +7,45 @@
     
     a)Para que serve o construtor dentro de uma classe e como fazemos para chamá-lo?
 
-          Contrutor serve para fazer novos objetos aparti de Classe.
+          Contrutor serve para fazer novos objetos a partir de Classe.
     
     b)Copie o código abaixo para o seu exercício de hoje; crie uma instância 
     dessa classe (dê o nome, cpf e idade que você quiser). 
     Quantas vezes a mensagem `"Chamando o construtor da classe User"` foi impressa no terminal?
+
+       Sim, uma vez.
     
     
     c)Como conseguimos ter acesso às propriedades privadas de uma classe?
+
+     usando getters();
+
+    ### Exercício 2
+    
+    Transforme a variável de tipo abaixo, chamada `Transaction` em uma classe `Transaction`. 
+    Ela deve conter as mesmas propriedades: *data*, *valor* e *descrição*. Agora, porém, 
+    todas elas devem ser **privadas**. Portanto, crie métodos (*getters*) para a leitura desses 
+    valores, tanto para essa classe quanto para a classe UserAccount. Crie uma instância dessa 
+    classe e adicione à instância já criada de UserAccount 
+    
+                              
+                              type Transaction = 
+                              {
+                                    description: string,
+                                    value: number,
+                                    date: string
+                              }
+
+    
+    
+                              class Transaction  {
+                              private description: string;
+                              private value: number;
+                              private date: string;
+
+                              constructor(date: string, value: number, description: string ){ 
+                                    this.date= date ;
+                                    this.value = value;
+                                    this.description = description;
+                              }
+                              }
--- modulo4/classes-encapsulamento/src/index.ts
+++ modulo4/classes-encapsulamento/src/index.ts
@@ -1,6 +1,98 @@
 import app from "./app";
-
-
-
 app.get("/")
 
+
+class Transaction  {
+   private description: string;
+   private value: number;
+   private date: string;
+
+   constructor(date: string, value: number, description: string ){ 
+       this.date= date ;
+       this.value = value;
+       this.description = description;
+   }
+   public getDescription(){
+         return this.description;
+   }
+   public getValue(){ 
+       return this.value;
+   }
+   public getDate (){ 
+       return this.date
+   }
+  }
+
+class UserAccount {
+    private cpf: string;
+    private name: string;
+    private age: number;
+    private balance: number = 0;
+    private transactions: Transaction[] = [];
+  
+    constructor(
+       cpf: string,
+       name: string,
+       age: number,
+    ) {
+       console.log("Chamando o construtor da classe UserAccount")
+       this.cpf = cpf;
+       this.name = name;
+       this.age = age;
+    }
+
+    public getCpf(){ 
+        return this.cpf
+    }
+    public getName(){ 
+        return this.name
+    }
+    public getAge(){
+        return this.age
+    }
+    public getBalance(){ 
+        return this.balance
+    }
+    public getTransactions(){ 
+        return this.transactions
+    }  
+    public setTransactions(transaction: Transaction){ 
+        this.transactions =  [...this.transactions  ,transaction]
+    }
+  }
+
+  const eu = new UserAccount("111.111,111-11", "Gabriel" , 39 )
+//   console.table( eu )
+
+
+  const transitionEu = new Transaction("13/04/2022" ,10 , "Arroz")
+//   console.table(transitionEu)
+
+  eu.setTransactions(transitionEu)
+//   console.log(eu)
+
+
+eu.setTransactions (new Transaction("20/04/2022" ,12 , "feijao"))
+// console.log(eu)
+
+
+
+// 3  
+
+class Bank { 
+   private accounts : UserAccount[];
+    constructor(accounts : UserAccount[]){ 
+        this.accounts = accounts;
+    }
+    public getAccounts(){ 
+        return this.accounts;
+    }
+}
+ 
+// console.log(eu)
+ const bancoDoBrasil  = new Bank([eu])
+
+console.log(bancoDoBrasil)
+const teste = bancoDoBrasil.getAccounts()
+
+console.log( "teste", teste)
