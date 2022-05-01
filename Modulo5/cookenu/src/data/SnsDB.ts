59db0940ee69313d40c1c4aff8e805a87d2cbb5c
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-05-01 21:38:51 +0800

    unfollowing DB class

--- Modulo5/cookenu/src/data/SnsDB.ts
+++ Modulo5/cookenu/src/data/SnsDB.ts
@@ -7,14 +7,24 @@ import { BaseDataBase } from "./BaseDataBase";
 export class SnsDB extends BaseDataBase {
 
     public async followingDB(followingUser: followingUser): Promise<void> {
-   try {
-           await BaseDataBase.connection("USER_SNS")
-               .insert(followingUser) 
-       
-   } catch (error:any) {
-
-     throw new Error(error.sqlmessage|| error.message );
+        try {
+            await BaseDataBase.connection("USER_SNS")
+                .insert(followingUser)
+        } catch (error: any) {
+            throw new Error(error.sqlmessage || error.message);
+        }
+    }
+    public async unfollowingDB(followedUserId: string , followingId: string ): Promise<void> {
+        try {
+            await BaseDataBase.connection("USER_SNS")
+                .where("fed_id", followedUserId)  // fed  = followed Id  
+                .andWhere("fing_id", followingId )   // fing = folling Id 
+                .del()
     
-   }
-}
+
+        } catch (error: any) {
+            throw new Error(error.sqlmessage || error.message);
+        }
+    }
+
 }
