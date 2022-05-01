52da7a17c2f3670d77d056cc38cea316d8da3a5b
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-05-01 21:37:32 +0800

    following changes

--- Modulo5/cookenu/src/index.ts
+++ Modulo5/cookenu/src/index.ts
@@ -1,20 +1,24 @@
 import {app} from "./app";
-import { CreatingFollowingUser } from "./endpoints/CreatingfollowingUser";
+import { CreatingFollowingUser } from "./endpoints/followingUser";
 import { creatingRecipes } from "./endpoints/creatingRecipes";
 import { feed } from "./endpoints/feed";
 import { gettingRecipeById } from "./endpoints/GettingRecipes";
 import { GettingUserInfo } from "./endpoints/GettingUserInfo";
 import { login } from "./endpoints/login";
 import { signup } from "./endpoints/signup";
+import { unFollowingUser } from "./endpoints/unfollowingUser";
 
 
+app.post("/signup", signup)
 app.post("/signup", signup)
 
 app.post("/login", login)
 
 app.get("/user/profile", GettingUserInfo)
 
-app.post("/user/:id", CreatingFollowingUser)
+app.post("/user/follow", CreatingFollowingUser)
+
+app.post("/user/unfollow", unFollowingUser)
 
 app.post("/recipe", creatingRecipes)
 
