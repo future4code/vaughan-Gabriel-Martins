8c33d995c6222b9ba1bcf94bd2709d9ebfe90d6b
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-05-01 16:51:08 +0800

    method Recipe - into class Recipes

--- Modulo5/cookenu/src/data/RecipesDB.ts
+++ Modulo5/cookenu/src/data/RecipesDB.ts
@@ -8,21 +8,28 @@ import { BaseDataBase } from "./BaseDataBase";
 export class RecipesDB extends BaseDataBase{ 
 
     public async createRecipe (recipe : Recipes):Promise<void> { 
-
        try {
           await BaseDataBase.connection("RECEITAS")
                                     .insert(recipe)
-
-
-           
        } catch (error : any) {
 
         throw new Error(error.sqlMessage || error.message);
-    
-           
        }
-
     }
+
+   public async gettingRecipeById (id: string ):Promise<Recipes[] > { 
+  try {
+        const result:Recipes[] = await BaseDataBase.connection("RECEITAS").where({id}).select() 
+        return result 
+
+       
+  } catch (error: any) {
+
+    throw new Error(error.sqlMessage || error.message);
+     
+  }
+    
+   }
 
 
 
