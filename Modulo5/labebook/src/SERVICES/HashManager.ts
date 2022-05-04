d24b371d93df5cba811ee021f90ab8da20b17f83
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-05-04 14:29:46 +0800

    HashManager  {    . hash   . compare    }

--- Modulo5/labebook/src/SERVICES/HashManager.ts
+++ Modulo5/labebook/src/SERVICES/HashManager.ts
@@ -11,7 +11,14 @@ import * as bcryptjs from "bcryptjs";
 
 export class HashManager { 
  
-    protected static hash (){ 
-        
+    protected static async hash  (text:string ){ 
+        const rounds = Number(process.env.HASH_COST)
+        const salt = await bcryptjs.genSalt(Number(rounds))
+        return bcryptjs.hash(text, salt)
     }
+
+    protected static async compare(text:string, hashDB: string):Promise<boolean>  {
+           return  bcryptjs.compare(text , hashDB )
+    }   
+    
 }
