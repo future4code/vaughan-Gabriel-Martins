9a0b1dddf3956b24896d4c1691a9609a146040ac
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-05-10 11:42:18 +0800

    exercicio 1 e 2

---
+++ Modulo5/testes-automatizados/jest.config.js
@@ -0,0 +1,9 @@
+const {defaults} = require('jest-config');
+module.exports = {
+    roots: ["<rootDir>/tests"],
+    transform: {
+      "^.+\\.tsx?$": "ts-jest",
+    },
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+   };
